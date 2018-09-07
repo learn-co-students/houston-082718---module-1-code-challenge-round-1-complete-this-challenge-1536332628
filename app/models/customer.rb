1247a2881333f36d177cf7926b05a4ce90558343
github username <github email address>
2018-09-07 11:10:46 -0500

    Done.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,81 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  # Customer.all : should return all of the customer instances
+  def self.all
+    @@all
+  end
+
+  # Customer.find_by_name(name) : given a string of a full name, 
+  # returns the first customer whose full name matches
+  def self.find_by_name(name)
+    self.all.find do |customer|
+      customer.first_name == name.split(" ").first && customer.last_name == name.split(" ").last
+    end
+  end
+
+  # Customer.find_all_by_first_name(name) : given a string of 
+  # a first name, returns an array containing all customers 
+  # with that first name
+  def self.find_all_by_first_name(name)
+    self.all.select do |customer|
+      customer.first_name == name
+    end
+  end
+
+  # Customer.all_names : should return an array of all 
+  # of the customer full names
+  def self.all_names
+    self.all.collect do |customer|
+      "#{customer.first_name} #{customer.last_name}"
+    end
+  end
+
+  # Customer#add_review(restaurant, content, rating) : given a 
+  # restaurant object, some review content (as a string), and 
+  # a star rating (as an integer), creates a new review and 
+  # associates it with that customer and restaurant.
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, rating, content)
+  end
+
+  # Customer#num_reviews : Returns the total number of reviews 
+  # that a customer has authored
+  def num_reviews
+    Review.all.select do |review|
+      review.customer == self
+    end.count
+  end
+
+  # Customer#restaurants : Returns a unique array of all 
+  # restaurants a customer has reviewed
+  def restaurants
+    array = Review.all.select do |review|
+      review.customer == self
+    end
+
+    array.collect do |review|
+      review.restaurant
+    end
+
+  end
 end
+
+
+
+
+
+
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,22 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+kim = Customer.new("K","P")
+michael = Customer.new("M","C")
+jordan = Customer.new("J","C")
+mj = Customer.new("M", "J")
+
+
+local_foods = Restaurant.new("Local Foods")
+lolas = Restaurant.new("Lola's")
+
+
+kim_review1 = Review.new(kim, local_foods, 5, "Good")
+michael_review1 = Review.new(michael, local_foods, 3, "OK")
+jordan_review1 = Review.new(jordan, local_foods, 2, "Bad")
+
+
+kim.add_review(lolas, "Good", 5)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
