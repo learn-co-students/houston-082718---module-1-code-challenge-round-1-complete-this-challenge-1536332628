ae30b8cbb61fc14e4dd8d490271be3e550dcf904
Naeblis <johnmlowery@gmail.com>
2018-09-07 11:07:28 -0500

    Done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,46 @@
+
+
+
+
 class Customer
+  @@all = []
   attr_accessor :first_name, :last_name
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+      
+  def self.all_names
+    array = []
+    self.all.collect do |names|
+      array << names.first_name
+    end
+    nuarray = self.all.collect do|names|
+      array << names.last_name
+    end
+    array
+  end
+
+
+  def self.find_all_by_first_name(name)
+    array = []
+    self.all.collect do |names|
+      if names.first_name == name
+        array << names.first_name
+      end
+    end
+    array
+  end
+
 end
+ 
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,28 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+
+
+
+john = Customer.new("John", "grubbs")
+kim = Customer.new("kim", "pham")
+
+ihop = Restaurant.new("ihop")
+
+review = Review.new(john, ihop, 5)
+
+
+
+
+
+
+
+
+
+
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
