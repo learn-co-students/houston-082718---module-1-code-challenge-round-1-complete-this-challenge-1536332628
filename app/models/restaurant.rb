245dfe6212ec7fc71ae45bda6d89d7a5cbba5ffd
EthanFe <sneakcow@gmail.com>
2018-09-07 10:40:40 -0500

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,40 @@
 class Restaurant
+  @@all = []
+
   attr_accessor :name
 
   def initialize(name)
     @name = name
+
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |restaurant|
+      restaurant.name == name
+    end
+  end
+
+  def reviews
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+    reviews.map {|review| review.customer}.uniq
+  end
+
+  def average_star_rating
+    total = 0
+    reviews.each {|review| total += review.rating}
+    total.to_f / reviews.length
+  end
+
+  def longest_review
+    sorted_reviews = reviews.sort_by {|review| review.content.length}
+    sorted_reviews[-1]
+  end
 end
